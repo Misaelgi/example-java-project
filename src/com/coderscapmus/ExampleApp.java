6a91e421f8c7bc08662309fafc30c7f080c15c6f
Misael Gallegos <misaelg2002@gmail.com>
2023-11-10 10:24:23 -0500

    added line

--- src/com/coderscapmus/ExampleApp.java
+++ src/com/coderscapmus/ExampleApp.java
@@ -3,8 +3,9 @@ package com.coderscapmus;
 public class ExampleApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Testing 123");
+		System.out.println("Testing some more");
 	}
 
 }
